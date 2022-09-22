79b977e5f5c340a34fe85e2c99e72a892b702761
n_narcise <joaquin.narcisse@gmail.com>
2022-09-22 11:28:34 -0500

    add 01.js

--- WILL-main/01.js
+++ WILL-main/01.js
@@ -11,6 +11,8 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
+  let numbers = array.filter(element => typeof element === 'number');;
+  return numbers;
 
 }
 
